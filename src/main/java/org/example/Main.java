7a4299614e3d5c9b17ffb2fa6f1e9357c59f13be
krishnakrmahto <krish.thorcode@gmail.com>
2023-09-12 00:48:04 +0530

    changed code to write to a file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,9 @@ package org.example;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flipkart.zjsonpatch.JsonDiff;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.stream.StreamSupport;
 
@@ -11,24 +13,51 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
 
-    ObjectMapper objectMapper = new ObjectMapper();
-
     File jsonFile1 = new File("src/main/resources/json1.txt");
     File jsonFile2 = new File("src/main/resources/json2.txt");
+
+    JsonNode jsonDiff = getJsonDiff(jsonFile1, jsonFile2);
+
+    StreamSupport.stream(jsonDiff.spliterator(), false)
+        .forEach(jsonDiffNode -> {
+
+          String completePrintString = getDiffKeyValuePair(jsonDiffNode);
+
+          File jsonDiffFile = new File("src/main/resources/json-diff.txt");
+          jsonDiffFile.delete();
+
+          try {
+
+            jsonDiffFile.createNewFile();
+
+            FileWriter fileWriter = new FileWriter(jsonDiffFile, true);
+            BufferedWriter bw = new BufferedWriter(fileWriter);
+
+            bw.write(completePrintString);
+            bw.close();
+
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
+        });
+  }
+
+  private static String getDiffKeyValuePair(JsonNode jsonDiffNode) {
+
+    String keyPrintString = "Key: " + jsonDiffNode.get("path").toString().replace("/", "");
+    String valuePrintString = "Value: " + jsonDiffNode.get("value");
+
+    return keyPrintString + "\n" + valuePrintString + "\n\n";
+  }
+
+  private static JsonNode getJsonDiff(File jsonFile1, File jsonFile2)
+      throws IOException {
+
+    ObjectMapper objectMapper = new ObjectMapper();
 
     JsonNode jsonNode1 = objectMapper.readTree(jsonFile1);
     JsonNode jsonNode2 = objectMapper.readTree(jsonFile2);
 
-    JsonNode jsonDiff = JsonDiff.asJson(jsonNode1, jsonNode2);
-
-    System.out.println();
-    System.out.println("Difference (path-value pairs): ");
-    StreamSupport.stream(jsonDiff.spliterator(), false)
-        .forEach(jsonDiffNode -> {
-          System.out.println("Path: " + jsonDiffNode.get("path").toString().replace("/", ""));
-          System.out.println("Value: " + jsonDiffNode.get("value"));
-        });
-
-    System.out.println();
+    return JsonDiff.asJson(jsonNode1, jsonNode2);
   }
 }
