8a9f5addacae6023f451debdaf9ccf6de71346c5
krishnakrmahto <krish.thorcode@gmail.com>
2023-09-11 17:00:03 +0530

    read json objects from file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,40 +1,34 @@
 package org.example;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flipkart.zjsonpatch.JsonDiff;
-import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 import java.util.stream.StreamSupport;
 
 public class Main {
 
-  public static void main(String[] args) throws JsonProcessingException {
-    Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) throws IOException {
 
-    while (true) {
-      System.out.println("Enter first json: ");
-      String json1 = sc.nextLine();
+    ObjectMapper objectMapper = new ObjectMapper();
 
-      System.out.println("Enter second json: ");
-      String json2 = sc.nextLine();
+    File jsonFile1 = new File("src/main/resources/json1.txt");
+    File jsonFile2 = new File("src/main/resources/json2.txt");
 
-      ObjectMapper objectMapper = new ObjectMapper();
+    JsonNode jsonNode1 = objectMapper.readTree(jsonFile1);
+    JsonNode jsonNode2 = objectMapper.readTree(jsonFile2);
 
-      JsonNode jsonNode1 = objectMapper.readTree(json1);
-      JsonNode jsonNode2 = objectMapper.readTree(json2);
+    JsonNode jsonDiff = JsonDiff.asJson(jsonNode1, jsonNode2);
 
-      JsonNode jsonDiff = JsonDiff.asJson(jsonNode1, jsonNode2);
+    System.out.println();
+    System.out.println("Difference (path-value pairs): ");
+    StreamSupport.stream(jsonDiff.spliterator(), false)
+        .forEach(jsonDiffNode -> {
+          System.out.println("Path: " + jsonDiffNode.get("path").toString().replace("/", ""));
+          System.out.println("Value: " + jsonDiffNode.get("value"));
+        });
 
-      System.out.println();
-      System.out.println("Difference (path-value pairs): ");
-      StreamSupport.stream(jsonDiff.spliterator(), false)
-          .forEach(jsonDiffNode -> {
-            System.out.println("Path: " + jsonDiffNode.get("path").toString().replace("/",""));
-            System.out.println("Value: " + jsonDiffNode.get("value"));
-          });
-
-      System.out.println();
-    }
+    System.out.println();
   }
 }
