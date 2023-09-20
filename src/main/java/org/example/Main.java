4b075a60bf4f6cacd91c7f9ee2e41e00b64d2e4a
krishnakrmahto <krish.thorcode@gmail.com>
2023-09-20 09:54:02 +0530

    added changes to print json1 as well as json2 values

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,13 @@ package org.example;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flipkart.zjsonpatch.DiffFlags;
 import com.flipkart.zjsonpatch.JsonDiff;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.EnumSet;
 import java.util.stream.StreamSupport;
 
 public class Main {
@@ -45,10 +47,32 @@ public class Main {
 
   private static String getDiffKeyValuePair(JsonNode jsonDiffNode) {
 
-    String keyPrintString = "Key: " + jsonDiffNode.get("path").toString().replace("/", "");
-    String valuePrintString = "Value: " + jsonDiffNode.get("value");
+    String operation = jsonDiffNode.get("op").textValue();
 
-    return keyPrintString + "\n" + valuePrintString + "\n\n";
+    String printableDiffText = null;
+    String keyPath = jsonDiffNode.get("path").textValue();
+
+    if (operation.equals("add")) {
+
+      String json1Value = jsonDiffNode.get("value").textValue();
+
+      printableDiffText = "Key path: " + keyPath + "\n" + "Value-1: " + json1Value + "\n" + "Value-2: " + null;
+
+    } else if (operation.equals("remove")) {
+
+      String json2Value = jsonDiffNode.get("value").textValue();
+
+      printableDiffText = "Key path: " + keyPath + "\n" + "Value-1: " + null + "\n" + "Value-2: " + json2Value;
+
+    } else if (operation.equals("replace")) {
+
+      String json1Value = jsonDiffNode.get("value").textValue();
+      String json2Value = jsonDiffNode.get("fromValue").textValue();
+
+      printableDiffText = "Key path: " + keyPath + "\n" + "Value-1: " + json1Value + "\n" +  "Value-2: " + json2Value;
+    }
+
+    return printableDiffText + "\n\n";
   }
 
   private static JsonNode getJsonDiff(File jsonFile1, File jsonFile2)
@@ -59,6 +83,6 @@ public class Main {
     JsonNode jsonNode1 = objectMapper.readTree(jsonFile1);
     JsonNode jsonNode2 = objectMapper.readTree(jsonFile2);
 
-    return JsonDiff.asJson(jsonNode1, jsonNode2);
+    return JsonDiff.asJson(jsonNode1, jsonNode2, EnumSet.of(DiffFlags.ADD_ORIGINAL_VALUE_ON_REPLACE));
   }
 }
