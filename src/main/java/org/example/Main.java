1ad2528ef7b24ab3339683623e0c6289ca6c016a
krishnakrmahto <krish.thorcode@gmail.com>
2023-09-20 11:16:19 +0530

    corrected reading value from json diff node

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,31 +18,50 @@ public class Main {
     File jsonFile1 = new File("src/main/resources/json1.txt");
     File jsonFile2 = new File("src/main/resources/json2.txt");
 
-    JsonNode jsonDiff = getJsonDiff(jsonFile1, jsonFile2);
+    JsonNode jsonDiffArray = getJsonDiff(jsonFile1, jsonFile2);
 
+    File jsonDiffFile = createJsonDiffFile();
 
-    File jsonDiffFile = new File("src/main/resources/json-diff.txt");
-    jsonDiffFile.delete();
+    File logFile = createLogFile();
 
-    jsonDiffFile.createNewFile();
-
-    StreamSupport.stream(jsonDiff.spliterator(), false)
+    StreamSupport.stream(jsonDiffArray.spliterator(), false)
         .forEach(jsonDiffNode -> {
 
-          String completePrintString = getDiffKeyValuePair(jsonDiffNode);
+          String jsonDiffPrintString = getDiffKeyValuePair(jsonDiffNode);
 
           try {
 
-            FileWriter fileWriter = new FileWriter(jsonDiffFile, true);
-            BufferedWriter bw = new BufferedWriter(fileWriter);
-
-            bw.write(completePrintString);
-            bw.close();
+            writeStringToFile(logFile, jsonDiffNode + "\n\n");
+            writeStringToFile(jsonDiffFile, jsonDiffPrintString);
 
           } catch (IOException e) {
             throw new RuntimeException(e);
           }
         });
+  }
+
+  private static void writeStringToFile(File logFile, String jsonDiffNode) throws IOException {
+    FileWriter logWriter = new FileWriter(logFile, true);
+    BufferedWriter bufferedLogWriter = new BufferedWriter(logWriter);
+
+    bufferedLogWriter.write(jsonDiffNode);
+    bufferedLogWriter.close();
+  }
+
+  private static File createLogFile() throws IOException {
+    File logFile = new File("src/main/resources/json-diff-logs.txt");
+    logFile.delete();
+
+    logFile.createNewFile();
+    return logFile;
+  }
+
+  private static File createJsonDiffFile() throws IOException {
+    File jsonDiffFile = new File("src/main/resources/json-diff.txt");
+    jsonDiffFile.delete();
+
+    jsonDiffFile.createNewFile();
+    return jsonDiffFile;
   }
 
   private static String getDiffKeyValuePair(JsonNode jsonDiffNode) {
@@ -54,22 +73,24 @@ public class Main {
 
     if (operation.equals("add")) {
 
-      String json1Value = jsonDiffNode.get("value").textValue();
+      String json1Value = jsonDiffNode.get("value").toString();
 
       printableDiffText = "Key path: " + keyPath + "\n" + "Value-1: " + json1Value + "\n" + "Value-2: " + null;
 
     } else if (operation.equals("remove")) {
 
-      String json2Value = jsonDiffNode.get("value").textValue();
+      String json2Value = jsonDiffNode.get("value").toString();
 
       printableDiffText = "Key path: " + keyPath + "\n" + "Value-1: " + null + "\n" + "Value-2: " + json2Value;
 
     } else if (operation.equals("replace")) {
 
-      String json1Value = jsonDiffNode.get("value").textValue();
-      String json2Value = jsonDiffNode.get("fromValue").textValue();
+      String json1Value = jsonDiffNode.get("value").toString();
+      String json2Value = jsonDiffNode.get("fromValue").toString();
 
       printableDiffText = "Key path: " + keyPath + "\n" + "Value-1: " + json1Value + "\n" +  "Value-2: " + json2Value;
+    } else {
+      printableDiffText = "Key path: " + keyPath;
     }
 
     return printableDiffText + "\n\n";
@@ -83,6 +104,8 @@ public class Main {
     JsonNode jsonNode1 = objectMapper.readTree(jsonFile1);
     JsonNode jsonNode2 = objectMapper.readTree(jsonFile2);
 
-    return JsonDiff.asJson(jsonNode1, jsonNode2, EnumSet.of(DiffFlags.ADD_ORIGINAL_VALUE_ON_REPLACE));
+    return JsonDiff.asJson(jsonNode1, jsonNode2,
+        EnumSet.of(DiffFlags.ADD_ORIGINAL_VALUE_ON_REPLACE, DiffFlags.OMIT_COPY_OPERATION,
+            DiffFlags.OMIT_MOVE_OPERATION));
   }
 }
