4d41f35b474160f9833b1bfd7fff02fc33bb9886
krishnakrmahto <krish.thorcode@gmail.com>
2023-09-25 10:01:19 +0530

    handled diff in discount list

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,42 +2,103 @@ package org.example;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.flipkart.zjsonpatch.DiffFlags;
 import com.flipkart.zjsonpatch.JsonDiff;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.StreamSupport;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
 
-    File jsonFile1 = new File("src/main/resources/json1.txt");
-    File jsonFile2 = new File("src/main/resources/json2.txt");
+    File jsonFile1 = new File("src/main/resources/json1.json");
+    File jsonFile2 = new File("src/main/resources/json2.json");
 
-    JsonNode jsonDiffArray = getJsonDiff(jsonFile1, jsonFile2);
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    JsonNode jsonNode1 = objectMapper.readTree(jsonFile1);
+    JsonNode jsonNode2 = objectMapper.readTree(jsonFile2);
 
     File jsonDiffFile = createJsonDiffFile();
-
     File logFile = createLogFile();
 
-    StreamSupport.stream(jsonDiffArray.spliterator(), false)
-        .forEach(jsonDiffNode -> {
+    Map<String, List<ObjectNode>> discountListWithKeyPath1 = removeAndGetDiscountListFromAssetList(
+        (ArrayNode) jsonNode1.get("assetList"));
 
-          String jsonDiffPrintString = getDiffKeyValuePair(jsonDiffNode);
+    Map<String, List<ObjectNode>> discountListWithKeyPath2 = removeAndGetDiscountListFromAssetList(
+        (ArrayNode) jsonNode2.get("assetList"));
 
-          try {
+    Map<String, Pair<ObjectNode,ObjectNode>> jsonPathWithNameToDiscountPairMap = getJsonPathWithNameToDiscountPairMap(discountListWithKeyPath1,
+        discountListWithKeyPath2);
 
-            writeStringToFile(logFile, jsonDiffNode + "\n\n");
-            writeStringToFile(jsonDiffFile, jsonDiffPrintString);
 
-          } catch (IOException e) {
-            throw new RuntimeException(e);
-          }
+    writeJsonDiffWithoutDiscountList(jsonNode1, jsonNode2, jsonDiffFile, logFile);
+
+    writeJsonDiffForDiscountList(jsonPathWithNameToDiscountPairMap, jsonDiffFile);
+
+  }
+
+  private static Map<String, Pair<ObjectNode, ObjectNode>> getJsonPathWithNameToDiscountPairMap(
+      Map<String, List<ObjectNode>> discountListWithKeyPath1, Map<String, List<ObjectNode>> discountListWithKeyPath2) {
+
+    Set<String> discountListPaths = discountListWithKeyPath1.keySet();
+
+    Map<String, Pair<ObjectNode, ObjectNode>> jsonPathWithNameToDiscountPairMap = new HashMap<>();
+
+    discountListPaths
+        .forEach(discountListPath -> {
+          List<ObjectNode> discountList1 = discountListWithKeyPath1.get(discountListPath);
+          List<ObjectNode> discountList2 = discountListWithKeyPath2.get(discountListPath);
+
+          discountList1.forEach(discount -> {
+            String name = discount.get("name").textValue();
+            String jsonPathWithName = discountListPath + "?name=" + name;
+
+            jsonPathWithNameToDiscountPairMap.put(jsonPathWithName, new Pair<>(discount, null));
+          });
+
+          discountList2.forEach(discount -> {
+            String name = discount.get("name").textValue();
+            String jsonPathWithName = discountListPath + "?name=" + name;
+
+            if (jsonPathWithNameToDiscountPairMap.containsKey(jsonPathWithName)) {
+              jsonPathWithNameToDiscountPairMap.get(jsonPathWithName).setSecond(discount);
+            } else {
+              jsonPathWithNameToDiscountPairMap.put(jsonPathWithName, new Pair<>(null, discount));
+            }
+          });
         });
+
+    return jsonPathWithNameToDiscountPairMap;
+  }
+
+  private static Map<String, List<ObjectNode>> removeAndGetDiscountListFromAssetList(ArrayNode assetList) {
+
+    Function<Integer, String> keyMapper = index ->  "/assetList" + "/" + index + "/discountList";
+    Function<Integer, List<ObjectNode>> valueMapper = index -> {
+      List<ObjectNode> value = new ArrayList<>();
+      ((ObjectNode) assetList.get(index)).remove("discountList").forEach(discount -> value.add((ObjectNode) discount));
+      return value;
+    };
+
+    return IntStream.range(0, assetList.size())
+        .boxed()
+        .collect(Collectors.toMap(keyMapper, valueMapper));
   }
 
   private static void writeStringToFile(File logFile, String jsonDiffNode) throws IOException {
@@ -64,31 +125,32 @@ public class Main {
     return jsonDiffFile;
   }
 
-  private static String getDiffKeyValuePair(JsonNode jsonDiffNode) {
+  private static String getPrintableDiffKeyValuePair(JsonNode jsonDiffNode, String pathPrefix) {
 
     String operation = jsonDiffNode.get("op").textValue();
 
-    String printableDiffText = null;
+    String printableDiffText;
     String keyPath = jsonDiffNode.get("path").textValue();
+    keyPath = (Optional.ofNullable(pathPrefix).isPresent()? pathPrefix: "") + keyPath;
 
     if (operation.equals("add")) {
 
       String json1Value = jsonDiffNode.get("value").toString();
 
-      printableDiffText = "Key path: " + keyPath + "\n" + "Value-1: " + json1Value + "\n" + "Value-2: " + null;
+      printableDiffText = getPrintableDiffText(keyPath, json1Value, null);
 
     } else if (operation.equals("remove")) {
 
       String json2Value = jsonDiffNode.get("value").toString();
 
-      printableDiffText = "Key path: " + keyPath + "\n" + "Value-1: " + null + "\n" + "Value-2: " + json2Value;
+      printableDiffText = getPrintableDiffText(keyPath, null, json2Value);
 
     } else if (operation.equals("replace")) {
 
       String json1Value = jsonDiffNode.get("value").toString();
       String json2Value = jsonDiffNode.get("fromValue").toString();
 
-      printableDiffText = "Key path: " + keyPath + "\n" + "Value-1: " + json1Value + "\n" +  "Value-2: " + json2Value;
+      printableDiffText = getPrintableDiffText(keyPath, json1Value, json2Value);
     } else {
       printableDiffText = "Key path: " + keyPath;
     }
@@ -96,13 +158,58 @@ public class Main {
     return printableDiffText + "\n\n";
   }
 
-  private static JsonNode getJsonDiff(File jsonFile1, File jsonFile2)
-      throws IOException {
+  private static void writeJsonDiffWithoutDiscountList(JsonNode jsonNode1, JsonNode jsonNode2, File jsonDiffFile,
+      File logFile) {
+    JsonNode jsonDiffArray = getJsonDiff(jsonNode1, jsonNode2);
 
-    ObjectMapper objectMapper = new ObjectMapper();
+    StreamSupport.stream(jsonDiffArray.spliterator(), false)
+        .forEach(jsonDiffNode -> {
 
-    JsonNode jsonNode1 = objectMapper.readTree(jsonFile1);
-    JsonNode jsonNode2 = objectMapper.readTree(jsonFile2);
+          String jsonDiffPrintString = getPrintableDiffKeyValuePair(jsonDiffNode, null);
+
+          try {
+
+            writeStringToFile(logFile, jsonDiffNode + "\n\n");
+            writeStringToFile(jsonDiffFile, jsonDiffPrintString);
+
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
+        });
+  }
+
+  private static void writeJsonDiffForDiscountList(Map<String, Pair<ObjectNode, ObjectNode>> jsonPathWithNameToDiscountPairMap,
+      File jsonDiffFile) {
+
+    jsonPathWithNameToDiscountPairMap.forEach((jsonPathWithName, discountPair) -> {
+
+      ObjectNode first = discountPair.getFirst();
+      ObjectNode second = discountPair.getSecond();
+
+      JsonNode jsonDiffArray = getJsonDiff(first, second);
+
+      StreamSupport.stream(jsonDiffArray.spliterator(), false)
+          .forEach(jsonDiffNode -> {
+
+            String jsonDiffPrintString = getPrintableDiffKeyValuePair(jsonDiffNode, jsonPathWithName);
+
+            try {
+
+              writeStringToFile(jsonDiffFile, jsonDiffPrintString);
+
+            } catch (IOException e) {
+              throw new RuntimeException(e);
+            }
+          });
+
+    });
+  }
+
+  private static String getPrintableDiffText(String keyPath, String json1Value, String json2Value) {
+    return "Key path: " + keyPath + "\n" + "Value-1: " + json1Value + "\n" + "Value-2: " + json2Value;
+  }
+
+  private static JsonNode getJsonDiff(JsonNode jsonNode1, JsonNode jsonNode2) {
 
     return JsonDiff.asJson(jsonNode1, jsonNode2,
         EnumSet.of(DiffFlags.ADD_ORIGINAL_VALUE_ON_REPLACE, DiffFlags.OMIT_COPY_OPERATION,
